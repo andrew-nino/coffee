47e8ad37aa0fd1ba45412b797da1675309574b23
andrew_nino <andrey.muravey1@gmail.com>
2022-11-18 04:01:04 +0300

    Basic structure of the project. HTTP Server

---
+++ cmd/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"coffee-app"
+	"coffee-app/pkg/handler"
+	"log"
+)
+
+func main() {
+
+	handlers := new(handler.Handler)
+
+	srv := new(coffee.Server)
+
+	if err := srv.Run("8080", handlers.InitRoutes()); err != nil {
+		log.Fatalf("error occured while running http server : %s", err.Error())
+	}
+}
